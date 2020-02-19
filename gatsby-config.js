81d014c5618b55921c3154f2f2f39635d4d68b27
Pedro Rojas <pedro.rojas@gmail.com>
2020-02-19 14:00:51 -0600

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,5 +43,25 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-4668429-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "resume.monoforms.com",
+      },
+    },
   ],
 }
