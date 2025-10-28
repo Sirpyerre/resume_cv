26816e7523b03fc2db8c2c76cc0e55bb131458d6
Pedro Rojas Reyes <pedro.rojas@gmail.com>
2025-10-28 13:25:35 -0600

    deleting and creating new files to react and vite

--- src/pages/contact.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-export default () => (
-  <div>
-    <h1>Contacto</h1>
-    <p>
-      <a href="mailto:peter@monoforms.com">peter(@)monoforms.com</a>
-    </p>
-  </div>
-)
